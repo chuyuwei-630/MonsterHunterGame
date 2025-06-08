acede62b519d9d7c6b4960ecc95916af348eb631
chuyuwei-630 <icctw.money@gmail.com>
2025-06-08 11:49:01 +0800

    修正過後的

--- Character.cpp
+++ Character.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Character.h"
 
 Character::Character(const std::string& name, int hp, int attack)
@@ -27,34 +26,3 @@ int Character::getHP() const {
 
 void Character::increaseAttack(int amount) {
     attack += amount;
-=======
-#include "Character.h"
-
-Character::Character(const std::string& name, int hp, int attack)
-    : name(name), hp(hp), attack(attack) {}
-
-void Character::takeDamage(int dmg) {
-    hp -= dmg;
-    if (hp < 0) hp = 0;
-}
-
-bool Character::isAlive() const {
-    return hp > 0;
-}
-
-int Character::getAttack() const {
-    return attack;
-}
-
-std::string Character::getName() const {
-    return name;
-}
-
-int Character::getHP() const {
-    return hp;
-}
-
-void Character::increaseAttack(int amount) {
-    attack += amount;
->>>>>>> 1375e4f2c47132a367752508f7f518dbf4a6ab4b
-}
