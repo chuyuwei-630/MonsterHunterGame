6c07ed8daf3dbf4949a50a5bb71952ed0a5a8090
Andy-weii <wwiiwwnnjjnn@gmail.com>
2025-06-10 13:10:30 +0800

    Update Equipment.h

--- Equipment.h
+++ Equipment.h
@@ -5,24 +5,22 @@
 
 class Equipment {
 public:
-    enum Type { WEAPON, ARMOR, ACCESSORY };
+    enum Type { WEAPON, ARMOR, ACCESSORY }; // 新增飾品類型
 
-    Equipment(const std::string& name, Type type, int attackBonus, int hpBonus, int defenseBonus, int hpRegen);
+    Equipment(const std::string& name, Type type, int attackBonus, int hpBonus, int defenseBonus = 0);
 
     std::string getName() const;
     Type getType() const;
     int getAttackBonus() const;
     int getHpBonus() const;
     int getDefenseBonus() const;
-    int getHpRegen() const;
 
 private:
     std::string name;
     Type type;
     int attackBonus;
     int hpBonus;
-    int defenseBonus;
-    int hpRegen;
+    int defenseBonus; // 新增防禦力加成
 };
 
 #endif // EQUIPMENT_H
