fad12557663731e4c2749839f04605228efe98d5
Andy-weii <wwiiwwnnjjnn@gmail.com>
2025-06-10 11:18:20 +0800

    Update Equipment.cpp

--- Equipment.cpp
+++ Equipment.cpp
@@ -1,7 +1,7 @@
 #include "Equipment.h"
 
-Equipment::Equipment(const std::string& name, Type type, int attackBonus, int hpBonus, int defenseBonus)
-    : name(name), type(type), attackBonus(attackBonus), hpBonus(hpBonus), defenseBonus(defenseBonus) {}
+Equipment::Equipment(const std::string& name, Type type, int attackBonus, int hpBonus, int defenseBonus, int hpRegen)
+    : name(name), type(type), attackBonus(attackBonus), hpBonus(hpBonus), defenseBonus(defenseBonus), hpRegen(hpRegen) {}
 
 std::string Equipment::getName() const {
     return name;
@@ -21,4 +21,8 @@ int Equipment::getHpBonus() const {
 
 int Equipment::getDefenseBonus() const {
     return defenseBonus;
+}
+
+int Equipment::getHpRegen() const {
+    return hpRegen;
 }
